9b33c7b369df7874893ff715197ba29a0e2d155d
Yash Chohtel <yashchohtel@gmail.com>
2025-07-27 22:58:00 +0530

    header started

---
+++ FRONTEND/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './header.css'
+import logo from "../assets/logo.png";
+
+const Header = () => {
+    return (
+        <>
+            {/* header */}
+            <div className="header">
+
+                {/* container wrapper */}
+                <div className="container">
+
+                    {/* logo */}
+                    <div className="logo">
+                        <img src="" alt="" />
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Header
